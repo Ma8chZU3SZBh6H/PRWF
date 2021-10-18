d09c880a77513afad0aadad4549dc83ef1b4004b
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-18 20:37:31 +0300

    cleaned up product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,28 +15,35 @@ class Product extends Model
         'price'
     ];
 
+
     private static $stuff = [
-        "clear" => ["Shirt", "Skirt", "light", "Underwear"],
-        "isolated-clouds" => ["Shirt", "Skirt", "light"],
-        "scattered-clouds" => ["Shirt", "Skirt", "light"],
-        "overcast" => ["Shirt", "Skirt", "light"],
-        "light-rain" => ["Umbrella", "Hat"],
-        "moderate-rain" => ["Umbrella", "Hat"],
-        "heavy-rain" => ["Umbrella", "Hat"],
-        "sleet" => ["warm", "Gloves"],
-        "light-snow " => ["warm", "Coat", "Gloves"],
-        "moderate-snow" => ["warm", "Coat", "Gloves"],
-        "heavy-snow" => ["Heavy", "Coat", "Gloves"],
-        "fog" => ["light"],
-        "na" => [""],
+        "clear isolated-clouds scattered-clouds" => ["light%Shirt", "Small%Shirt", "Original%Shirt", "Small%Shirt", "light%Skirt", "Small%Skirt", "Original%Skirt", "Small%Skirt"],
+        "overcast light-rain moderate-rain heavy-rain" => ["Umbrella", "Hat"],
+        "sleet light-snow moderate-snow heavy-snow" => ["Heavy%Hat", "Heavy%Gloves", "Heavy%Coat", "Heavy%Coat"],
+        "fog" => ["Suspicious%Underwear"],
+        "na" => ["%"]
     ];
+
+    static private function select_proper_items($weather)
+    {
+        $stuff = null;
+        foreach (static::$stuff as $key => $value) {
+            if (str_contains($key, $weather)) {
+                $stuff = $value;
+            }
+        }
+        return $stuff;
+    }
 
     static public function select_by_weather($weather)
     {
-        $props = static::$stuff[$weather];
-        $q = static::where("name", "like", "%{$props[0]}%");
-        for ($i = 1; $i < 2; $i++) {
-            $q->orWhere("name", "like", "%{$props[$i]}%");
+        $stuff = static::select_proper_items($weather);
+
+        $q = static::where("name", "like", "%{$stuff[0]}%");
+        if (isset($stuff[1])) {
+            for ($i = 1; $i < 2; $i++) {
+                $q->orWhere("name", "like", "%{$stuff[$i]}%");
+            }
         }
         return $q->get()->random(2);
     }
